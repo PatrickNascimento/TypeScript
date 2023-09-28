b92ffcc762cc9472343748c130f066d0ba0c3833
Patrick <psn1462@gmail.com>
2023-09-28 09:00:59 -0300

    Initial commit

---
+++ CourseUdemy/index.js
@@ -0,0 +1,7 @@
+var firstName = "Patrick";
+var middleName = 1;
+function greeting(name) {
+    console.log("ola", name);
+}
+greeting(firstName);
+greeting(middleName);
---
+++ CourseUdemy/index.ts
@@ -0,0 +1,8 @@
+const firstName = "Patrick"
+const middleName = 1
+
+function greeting(name: string) {
+  console.log("ola", name);
+}
+
+greeting();
