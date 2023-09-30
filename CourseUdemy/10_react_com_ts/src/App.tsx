8a997af6a46e7c1db753f36c34fb4afa5dbd4ac1
Patrick <psn1462@gmail.com>
2023-09-30 18:40:16 -0300

    feat: variaveis
    variaveis ts react

--- CourseUdemy/10_react_com_ts/src/App.tsx
+++ CourseUdemy/10_react_com_ts/src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  // 1 Variaveis
+
+  const name: string = "Patrick";
+  const age: number = 41;
+  
+  const isworking: boolean = true;
+
   return (
     <div className="App">
-     <h1>React com TypeScript</h1>
+      <h1>React com TypeScript ®</h1>
+      <h2>{name}</h2>
+      <p>{age}</p>
+      {isworking && (
+        <div>
+          <p>Está trabalhando</p>
+        </div>
+      )}
     </div>
   );
 }
