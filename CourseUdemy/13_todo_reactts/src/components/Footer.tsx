a55df2690f5d342a8fc9d439a3ef1e94d3aff624
Patrick <psn1462@gmail.com>
2023-10-19 17:09:40 -0300

    feat: finish
    Finish Course

--- CourseUdemy/13_todo_reactts/src/components/Footer.tsx
+++ CourseUdemy/13_todo_reactts/src/components/Footer.tsx
@@ -1,12 +1,15 @@
-import styles from "./Footer.module.css"
+import React from "react";
+
+import styles from "./Footer.module.css";
+
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-    <p>React Ts ® 2023</p>
-  </footer>
-  )
+      <p>
+        <span>React + TS Todo</span> @ 2021
+      </p>
+    </footer>
+  );
 };
 
-export default Footer
-
-
+export default Footer;
--- CourseUdemy/13_todo_reactts/src/components/Header.tsx
+++ CourseUdemy/13_todo_reactts/src/components/Header.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import styles from "./Header.module.css"
 
-export default class Header extends React.Component {
-  public render() {
-    return (
-        <header className="styles.header">
-        <h1>React +TS Todo</h1>
-      </header>
-    );
-  }
-}
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>React + TS Todo</h1>
+    </header>
+  );
+};
+
+export default Header;
